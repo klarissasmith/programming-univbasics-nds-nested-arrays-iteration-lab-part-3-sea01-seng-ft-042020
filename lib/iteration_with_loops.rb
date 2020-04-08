7cdebefc42bd5d2a0ae4aa4db5a43ac456771f64
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-08 19:40:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,8 +8,8 @@ while row_index < src.count do
   while element_index < src[row_index].count do
    
     if src[row_index][element_index] > 0 
-      
-    endwords.push(src[row_index][element_index]) 
+      end
+    words.push(src[row_index][element_index]) 
     element_index += 1
     end
   row_index += 1
